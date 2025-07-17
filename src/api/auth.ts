5383d991b70d832ad2673fc88b7ed13766202284
mr_sdso <alex951224@gmail.com>
2025-07-17 08:36:05 +0300

    Delete src directory

--- src/api/auth.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'http://192.168.50.116:8000/api/token/';
-
-export async function login(username: string, password: string) {
-  return axios.post(API_URL, { username, password });
-}
--- src/theme/theme.ts
+++
@@ -1,5 +0,0 @@
-import { MD3DarkTheme } from 'react-native-paper';
-export const theme = {
-  ...MD3DarkTheme,
-  // можно добавить свои цвета
-};
